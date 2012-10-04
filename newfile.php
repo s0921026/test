1398ae8656d2872f7d6292270527e619bed12167
s0921026 <bladdestiny2@yahoo.co.jp>
2012-10-04 16:16:53 +0900

    はろーわーく

---
+++ newfile.php
@@ -0,0 +1,28 @@
+<?php
+
+$counter_file = 'counter.txt';
+$counter_lenght = 8;
+
+$fp = fopen($counter_file, 'r+');
+
+if ($fp){
+	if (flock($fp, LOCK_EX)){
+
+		$counter = fgets($fp, $counter_lenght);
+		$counter++;
+
+		rewind($fp);
+
+		if (fwrite($fp,  $counter) === FALSE){
+			print('ファイル書き込みに失敗しました');
+		}
+
+		flock($fp, LOCK_UN);
+	}
+}
+
+fclose($fp);
+
+print('count:'.$counter);
+
+?>
